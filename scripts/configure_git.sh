c2b414c07d96f6236a70a042f3f30393dd810781
Kenny Luong <luong97@hawaii.edu>
2013-12-17 20:44:18 -0600

    Enabled git syntax highlighting in config script

--- scripts/configure_git.sh
+++ scripts/configure_git.sh
@@ -1,4 +1,8 @@
 git config --global user.name "Kenny Luong"
 git config --global user.email "luong97@hawaii.edu"
 git config --global core.editor "vim"
+git config --global color.ui true
+git config --global color.diff true
+
+
 
